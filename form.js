7d6065a9b3ac71885526e53e0313e5b6d56acc45
gustavovni08 <gustavovni08@gmail.com>
2022-03-24 10:59:09 -0300

    JS functions

--- form.js
+++ form.js
@@ -1,54 +1,66 @@
+jQuery(function($){
+    $("#telefone").mask("+99 (99) 99999-9999")
+})
+
+
+
 function checar() {
 
     var nome = document.getElementById("nome").value
+    
     var s1 = document.getElementById("senha_1").value
     var s2 = document.getElementById("senha_2").value    
+    
     var e1 = document.getElementById("email_1").value
     var e2 = document.getElementById("email_2").value
+
     var data = document.getElementById("data").value
 
-
-   if (nome == ""){       
-        
-        alert("porfavor, digite o seu nome")
-   
-    }
-
-   if (e1 == "" || e2 == ""){       
-      
-        alert("porfavor, digite o seu email")
-
-    }
-
-    if (s1 == "" || s2 == ""){       
-      
-        alert("porfavor, digite sua senha")
-
-    }
-
-
-    if (data == ""){       
-        
-        alert("porfavor, informe sua data de nascimento")
-   
-    }
-   
    
     if(s1.length < 8 ){
-
         alert("sua senha deve conter no minimo 8 caracteres")
+    }
     
-    } 
+    var upper = /[A-Z]/;
+    var down = /[a-z]/; 
+    var num = /[0-9]/;
+    var spc = /[!|@|#|$|%|^|&|*|(|)|-|_]/;
+
+
+    if(down.test(s1)){
+       return
+    } else {
+        alert('sua senha deve conter caracteres em minusculo')
+    }
+
+    if(upper.test(s1)){
+        return
+     } else {
+         alert('sua senha deve conter caracteres em minusculo')
+     }
+
+     if(num.test(s1)){
+        return
+     } else {
+         alert('sua senha deve conter caracteres em minusculo')
+     }
+
+     if(spc.test(s1)){
+        alert('sua senha não pode conter caracteres especiais ex: !|@|#|$|%|^|&|*|(|)|-|_')
+     }
+
     
     if( e1 != e2 || s1 != s2){
-
         alert("email ou senha não conferem")
-    
     }
+
+    if(cb != "True"){
+        alert('você deve aceitar os termos de serviço para realizar o cadastro')
+    }
+
 
 
 }
-
 
 function exibir() {
     
